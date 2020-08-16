cc743daa11c97a8c7d3ac28b9e4883cb4ede9c9a
Seino Yoshinori <pek9dkl@gmail.com>
2020-08-16 23:19:47 +0900

    initial commit

---
+++ lib/lichee_nano_nerves_sample.ex
@@ -0,0 +1,18 @@
+defmodule LicheeNanoNervesSample do
+  @moduledoc """
+  Documentation for LicheeNanoNervesSample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LicheeNanoNervesSample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lichee_nano_nerves_sample_test.exs
@@ -0,0 +1,8 @@
+defmodule LicheeNanoNervesSampleTest do
+  use ExUnit.Case
+  doctest LicheeNanoNervesSample
+
+  test "greets the world" do
+    assert LicheeNanoNervesSample.hello() == :world
+  end
+end
