f7704eb2e601775e91d7a253cc7094295d0c3b7a
Seino Yoshinori <pek9dkl@gmail.com>
2020-08-16 23:38:06 +0900

    environment: lichee nano用ファームをビルドするためにツール、ライブラリ、フレームワークのバージョンを調整した

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LicheeNanoNervesSample.MixProject do
 
   @app :lichee_nano_nerves_sample
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
+  @all_targets [:x86_64, :lichee_nano]
 
   def project do
     [
@@ -49,14 +49,8 @@ defmodule LicheeNanoNervesSample.MixProject do
       {:nerves_init_gadget, "~> 0.4", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.10", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.10", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.10", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.10", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.10", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.10", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.5", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.10", runtime: false, targets: :x86_64},
+      {:nerves_system_x86_64, "~> 1.10.0", runtime: false, targets: :x86_64},
+      {:nerves_system_lichee_nano, "~> 0.1.0", runtime: false, targets: :lichee_nano},
     ]
   end
 
